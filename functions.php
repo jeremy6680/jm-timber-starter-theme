bf548e5c9f06023818bed58a6746d8d59bb91fb7
Jeremy Marchandeau <jeremy.marchandeau@hotmail.fr>
2017-03-19 14:10:18 +0100

    option page activated and acf synchronization thanks to acf-json folder

--- functions.php
+++ functions.php
@@ -65,3 +65,18 @@ class StarterSite extends TimberSite {
 }
 
 new StarterSite();
+
+/* Create an options page */
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page();
+	
+}
+
+/* Use options info site wide — cf http://timber.github.io/timber/#acf-cookbook */
+add_filter( 'timber_context', 'mytheme_timber_context'  );
+
+function mytheme_timber_context( $context ) {
+    $context['options'] = get_fields('option');
+    return $context;
+}
